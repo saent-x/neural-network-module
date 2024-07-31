1b069befe605a05afe5ad630f6e2ae96e513ca9e
saent-x <torsxperience@outlook.com>
2024-07-31 14:54:42 +0100

    feat: added layer class with funcs to link layers and calc node sigmoid in layer

---
+++ pkg/Layer.cs
@@ -0,0 +1,30 @@
+namespace IDS_NN.pkg;
+
+public class Layer{
+    public List<Node> Nodes { get; set; } = [];
+
+    public Layer(double width) {
+        for(var i = 0; i < width; i++) {
+            var node = new Node(
+                // Mathematical Sigmoid formula
+                (double x) => (10 / (1 + Math.Pow(Math.E, -1 * x))) - 5,
+                0
+            );
+
+            Nodes.Add(node);
+        }
+    }
+
+    public void Calc(){
+        foreach (var node in Nodes) node.Calc();
+    }
+
+    public void Link(Layer nextLayer){
+        foreach (var nextNode in nextLayer.Nodes)
+        {
+            foreach(var node in Nodes){
+                nextNode.Add(node);
+            }
+        }
+    }
+}
