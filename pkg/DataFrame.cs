2b52d97a6bd3068590ce63a29b15cc27aa31524e
saent-x <torsxperience@outlook.com>
2024-08-01 15:41:48 +0100

    feat: Dataframe class to handle datasets

---
+++ pkg/DataFrame.cs
@@ -0,0 +1,6 @@
+namespace IDS_NN.pkg;
+
+public class DataFrame{
+    public List<double>? Inputs { get; set; }
+    public List<double>? Targets { get; set; }
+}
--- pkg/Dataset.cs
+++
@@ -1,6 +0,0 @@
-namespace IDS_NN.pkg;
-
-public class Dataset{
-    public List<double> Inputs { get; set; }
-    public List<double> Outputs { get; set; }
-}
