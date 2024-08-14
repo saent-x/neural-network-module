d9bcde3fa3f4851d5e6831e3ab21d695b41933f7
saent-x <torsxperience@outlook.com>
2024-08-14 09:05:33 -0400

    feat: testing out ReLU activation function

--- Program.cs
+++ Program.cs
@@ -4,16 +4,30 @@ using PandasNet;
 using NumSharp;
 using IDS_NN.core;
 
+
 // set seed state
 np.random.seed(0);
 
-var X = new float[3,4]{
-	{1,2,3, 2.5f},
-	{2.0f, 5.0f, -1.0f, 2.0f},
-	{-1.5f, 2.7f, 3.3f, -0.8f}
-};
+// var X = new float[3,4]{
+// 	{1,2,3, 2.5f},
+// 	{2.0f, 5.0f, -1.0f, 2.0f},
+// 	{-1.5f, 2.7f, 3.3f, -0.8f}	
+// };
 
-var layer_1 = new LayerDense(4, 5);
-var layer_2 = new LayerDense(5, 2);
+// var layer_1 = new LayerDense(4, 5);
+// var layer_2 = new LayerDense(5, 2);
 
-layer_1.Forward(X);
+// layer_1.Forward(X);
+// //Console.WriteLine($"layer_1: [{layer_1.Output}]");
+
+// layer_2.Forward(layer_1.Output);
+// Console.WriteLine($"layer_2: [{layer_2.Output}]");
+
+var inputs = new float[]{ 0,2, -1, 3.3f, -2.7f, 1.1f, 2.2f, -100 };
+List<float> output = [];
+
+foreach(var i in inputs) output.Add(Math.Max(0, i));
+
+Console.Write("[ ");
+output.ForEach(x => Console.Write($"{x}, "));
+Console.Write(" ]\n");
--- core/layers.cs
+++ core/layers.cs
@@ -1,6 +1,5 @@
 using PandasNet;
 using NumSharp;
-using Serilog.Debugging;
 
 namespace IDS_NN.core;
 
@@ -13,11 +12,13 @@ public class LayerDense(int n_inputs, int n_neurons)
 	/**
 		multiply the _weights by the 0.10 to create a gaussian distribution bounded by zero
 	**/
-	private NDArray _weights = 0.10 * np.random.randn(n_inputs, n_neurons);
-	private NDArray _biases = np.zeros((1, n_neurons));
+	private readonly NDArray _weights = 0.10 * np.random.randn(n_inputs, n_neurons);
+	private readonly NDArray _biases = np.zeros((1, n_neurons));
 	private NDArray? _output;
+	public NDArray Output => _output ?? 0;
 	
-	public void Forward(float[,] inputs)
+	
+	public void Forward(NDArray  inputs)
 	{
 		_output = np.dot(inputs, _weights) + _biases;
 	}
