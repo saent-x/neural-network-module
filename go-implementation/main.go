11510b41ebbc8bab10f1c497434c4959012f8a5f
saent-x <torsxperience@outlook.com>
2024-08-22 18:29:02 -0400

    feat: testing for softmax activation function

--- go-implementation/main.go
+++ go-implementation/main.go
@@ -2,23 +2,28 @@ package main
 
 import (
 	"fmt"
-	"github.com/saent-x/ids-nn/core"
+	"github.com/saent-x/ids-nn/core/activation"
 	"gonum.org/v1/gonum/mat"
 )
 
 func main() {
-	X, _ := core.SpiralData(100, 3)
+	sm := new(activation.SoftMax)
+	data := []float64{-2, -1, 0}
 
-	layer_1 := core.CreateLayer(2, 3)
-	layer_1.Forward(X)
+	sm.Forward(mat.NewDense(1, 3, data))
+	fmt.Println(mat.Formatted(sm.Output))
 
-	layer_2 := core.CreateLayer(3, 30)
-	layer_2.Forward(layer_1.Output)
-
-	activation_1 := new(core.ActivationReLU)
-	activation_1.Forward(layer_1.Output)
-
-	fmt.Println(mat.Formatted(activation_1.Output))
+	//X, _ := core.SpiralData(100, 3)
+	//
+	//layer_1 := core.CreateLayer(2, 3)
+	//layer_2 := core.CreateLayer(3, 30)
+	//activation_1 := new(activation.ReLU)
+	//
+	//layer_1.Forward(X)
+	//layer_2.Forward(layer_1.Output)
+	//activation_1.Forward(layer_1.Output)
+	//
+	//fmt.Println(mat.Formatted(activation_1.Output))
 
 	//fmt.Println(mat.Formatted(X))
 	//fmt.Println(y)
