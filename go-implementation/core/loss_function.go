793ae4ffd236588005cfacb79ed2fbc2b612074d
saent-x <torsxperience@outlook.com>
2024-08-28 09:40:32 -0400

    feat: implemented a loss function (Categorical Cross Entropy) to quantify how wrong the model is

---
+++ go-implementation/core/loss_function.go
@@ -0,0 +1,26 @@
+package core
+
+import (
+	"gonum.org/v1/gonum/mat"
+	"math"
+)
+
+// Loss function is used to quantify how wrong the model is.
+// Categorical Cross Entropy is also one of the most commonly used loss functions with a softmax activation
+// on the output layer. It works by comparing two probability distribution (predictions & targets)
+
+type LossFunction struct {
+	Loss float64
+}
+
+func (lf *LossFunction) Calc(softmax_output *mat.Dense, target_values *mat.Dense) {
+	rows, columns := softmax_output.Dims()
+	result := mat.NewDense(rows, columns, nil)
+
+	result.Apply(func(i, j int, value float64) float64 {
+		return math.Log(value) * target_values.At(i, j)
+	}, softmax_output)
+
+	// sum matrix all up
+	lf.Loss = -mat.Sum(result)
+}
