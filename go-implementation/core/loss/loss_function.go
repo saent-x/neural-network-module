2073318d15c68ef2362f5758b7eb503e9ecf25a1
saent-x <torsxperience@outlook.com>
2024-09-03 10:19:25 -0400

    feat: fixed issue with loss function calculation in multi-dimensional array

--- go-implementation/core/loss/loss_function.go
+++ go-implementation/core/loss/loss_function.go
@@ -1,26 +1,68 @@
 package loss
 
 import (
+	"github.com/samber/lo"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
+	"log"
 	"math"
 )
 
 // Loss function is used to quantify how wrong the model is.
 // Categorical Cross Entropy is also one of the most commonly used loss functions with a softmax activation
 // on the output layer. It works by comparing two probability distribution (predictions & targets)
+type ILoss interface {
+	Calculate(output *mat.Dense, y *mat.Dense) float64
+	forward(output *mat.Dense, y *mat.Dense)
+}
 
-type LossFunction struct {
+type CrossEntropyLossFunction struct {
 	Loss float64
 }
 
-func (lf *LossFunction) Calc(softmax_output *mat.Dense, target_values *mat.Dense) {
-	rows, columns := softmax_output.Dims()
-	result := mat.NewDense(rows, columns, nil)
+func (lf *CrossEntropyLossFunction) Calculate(output *mat.Dense, y *mat.Dense) float64 {
+	sample_losses := lf.forward(output, y)
+	average_loss := stat.Mean(sample_losses, nil)
 
-	result.Apply(func(i, j int, value float64) float64 {
-		return math.Log(value) * target_values.At(i, j)
-	}, softmax_output)
+	return average_loss
+}
 
-	// sum matrix all up
-	lf.Loss = -mat.Sum(result)
+func (lf *CrossEntropyLossFunction) forward(y_pred *mat.Dense, y_true *mat.Dense) []float64 {
+	samples, columns := y_pred.Dims()
+	rows, _ := y_true.Dims()
+
+	y_pred_clipped := mat.NewDense(samples, columns, nil)
+	y_pred_clipped.Apply(func(i, j int, value float64) float64 {
+		return lo.Clamp(value, 1e-7, 1-1e-7)
+	}, y_pred)
+
+	if rows == 1 {
+		var correct_confidences []float64
+
+		lo.ForEach(lo.Range(samples), func(item int, index int) {
+			value := y_pred_clipped.At(index, int(y_true.RawMatrix().Data[index])) // given that the data contained in y_true is one-hot encoded (0,1)
+			correct_confidences = append(correct_confidences, -math.Log(value))
+		})
+
+		return correct_confidences
+
+	} else if rows > 1 {
+		// for hot-one encoded categorical variables
+		var correct_confidences []float64
+
+		y_pred_y_true_product := mat.NewDense(samples, columns, nil)
+
+		y_pred_y_true_product.Mul(y_pred_clipped, y_true)
+
+		// sum each row and calc the natural logarithm of the sum
+		lo.ForEach(lo.Range(samples), func(item int, index int) {
+			row := y_pred_y_true_product.RawRowView(index)
+			correct_confidences = append(correct_confidences, -math.Log(lo.Sum(row)))
+		})
+
+		return correct_confidences
+	} else {
+		log.Fatalln("an error occurred")
+		return nil
+	}
 }
