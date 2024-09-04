901148804bd1f333fcf9caaf91c3333b4d8e4202
saent-x <torsxperience@outlook.com>
2024-09-04 11:40:53 -0400

    test: tested the vertical dataset and plotted scatter plot

--- go-implementation/core/utilities_test.go
+++ go-implementation/core/utilities_test.go
@@ -1,21 +1,47 @@
 package core
 
 import (
+	"fmt"
+	"gonum.org/v1/gonum/mat"
 	"testing"
 )
 
 func TestSpiralData(t *testing.T) {
 	X, y := SpiralData(100, 3)
 
-	if X == nil || len(y) == 0 {
+	fmt.Println(mat.Formatted(X))
+	fmt.Println(mat.Formatted(y))
+
+	if X == nil || y == nil {
 		t.Errorf("error: X & y spiral data is empty")
 	}
 }
 
-func TestScatterPlotFunction(t *testing.T) {
+func TestVerticalData(t *testing.T) {
+	X, y := VerticalData(100, 3)
+
+	fmt.Println(mat.Formatted(X))
+	fmt.Println(mat.Formatted(y))
+
+	if X == nil || y == nil {
+		t.Errorf("error: X & y vertical data is empty")
+	}
+}
+
+func TestScatterPlotFunctionForSpiralData(t *testing.T) {
 	X, _ := SpiralData(100, 3)
 
-	result := PlotData(X, 100, 3)
+	result := PlotData(X, 100, 3, "spiral")
+
+	if !result {
+		t.Errorf("error: no plot was made")
+	}
+}
+
+func TestScatterPlotFunctionForVerticalData(t *testing.T) {
+	X, _ := VerticalData(100, 3)
+
+	result := PlotData(X, 100, 3, "vertical")
 
 	if !result {
 		t.Errorf("error: no plot was made")
