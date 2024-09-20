4df1adebae0fc0f3f24957eaa79b1491316c9a84
saent-x <torsxperience@outlook.com>
2024-09-20 08:30:10 -0400

    test: wrote tests for NN layers

--- go-implementation/core/layer/layer_test.go
+++ go-implementation/core/layer/layer_test.go
@@ -6,16 +6,46 @@ import (
 	"testing"
 )
 
-func TestLayerCreation(t *testing.T) {
-	X := mat.NewDense(4, 3, []float64{0.7, 0.1, 0.2, 0.1, 0.5, 0.4, 0.02, 0.9, 0.08, 0.02, 0.9, 0.08})
+func TestReLUBackwardFunction(t *testing.T) {
+	inputs := mat.NewDense(3, 2, []float64{
+		1.0, 2.0,
+		0.5, -0.5,
+		-1.0, 2.0,
+	})
+	weights := mat.NewDense(2, 3, []float64{
+		0.1, 0.2, 0.3,
+		0.4, 0.5, 0.6,
+	})
+	biases := mat.NewDense(1, 3, []float64{0.0, 0.0, 0.0})
+	dvalues := mat.NewDense(3, 3, []float64{
+		0.1, -0.2, 0.3,
+		0.4, -0.5, 0.6,
+		-0.7, 0.8, -0.9,
+	})
 
-	layer_1 := CreateLayer(3, 3)
-	layer_2 := CreateLayer(3, 3)
-
-	layer_1.Forward(X)
-	fmt.Println(mat.Formatted(layer_1.Output))
-
-	if layer_1 == nil || layer_2 == nil {
-		t.Errorf("error: layer_1 & layer_2 are nil!")
+	layer := Layer{
+		Inputs:  inputs,
+		Weights: weights,
+		Biases:  biases,
 	}
+
+	layer.Backward(dvalues)
+
+	fmt.Printf("DWeights:\n%v\n", mat.Formatted(layer.D_Weights, mat.Prefix(" "), mat.Squeeze()))
+	fmt.Printf("DBiases:\n%v\n", mat.Formatted(layer.D_Biases, mat.Prefix(" "), mat.Squeeze()))
+	fmt.Printf("DInputs:\n%v\n", mat.Formatted(layer.D_Inputs, mat.Prefix(" "), mat.Squeeze()))
+}
+
+func TestSimple(t *testing.T) {
+	a := mat.NewDense(2, 2, []float64{1, 2, 3, 4})
+	b := mat.NewDense(2, 2, []float64{4, 3, 2, 1})
+
+	fmt.Println(mat.Formatted(a))
+	fmt.Println(mat.Formatted(b))
+
+	fmt.Printf("\nafter\n")
+	b = mat.DenseCopyOf(a)
+
+	fmt.Println(mat.Formatted(a))
+	fmt.Println(mat.Formatted(b))
 }
