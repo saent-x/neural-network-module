75f500d9df2bb7303b9d32b469ec1ab8a82bea94
saent-x <torsxperience@outlook.com>
2024-09-21 11:45:23 -0400

    misc: moved layer test to layer module

--- go-implementation/core/layer/layer_test.go
+++ go-implementation/core/layer/layer_test.go
@@ -2,6 +2,8 @@ package layer
 
 import (
 	"fmt"
+	"github.com/saent-x/ids-nn/core/activation"
+	"github.com/saent-x/ids-nn/core/optimization"
 	"gonum.org/v1/gonum/mat"
 	"testing"
 )
@@ -36,16 +38,34 @@ func TestReLUBackwardFunction(t *testing.T) {
 	fmt.Printf("DInputs:\n%v\n", mat.Formatted(layer.D_Inputs, mat.Prefix(" "), mat.Squeeze()))
 }
 
-func TestSimple(t *testing.T) {
-	a := mat.NewDense(2, 2, []float64{1, 2, 3, 4})
-	b := mat.NewDense(2, 2, []float64{4, 3, 2, 1})
+func TestLayerCreation(t *testing.T) {
+	X := mat.NewDense(4, 3, []float64{0.7, 0.1, 0.2, 0.1, 0.5, 0.4, 0.02, 0.9, 0.08, 0.02, 0.9, 0.08})
 
-	fmt.Println(mat.Formatted(a))
-	fmt.Println(mat.Formatted(b))
+	layer_1 := CreateLayer(3, 3)
+	layer_2 := CreateLayer(3, 3)
 
-	fmt.Printf("\nafter\n")
-	b = mat.DenseCopyOf(a)
+	activation_1 := new(activation.ReLU)
+	opt := optimization.CreateStochasticGradientDescent(1.0, 0.02, .5)
 
-	fmt.Println(mat.Formatted(a))
-	fmt.Println(mat.Formatted(b))
+	layer_1.Forward(X)
+	fmt.Println(mat.Formatted(layer_1.Output))
+	fmt.Println(" ")
+
+	activation_1.Forward(layer_1.Output)
+	fmt.Println("activation output: ", mat.Formatted(activation_1.Output))
+
+	activation_1.Backward(activation_1.Output)
+	fmt.Println("activation dinputs: ", mat.Formatted(activation_1.D_Inputs))
+	layer_1.Backward(activation_1.D_Inputs)
+
+	fmt.Println("layer 1 dinputs: ", mat.Formatted(layer_1.D_Inputs))
+
+	opt.UpdateParams(layer_1)
+	fmt.Println(" ")
+	fmt.Println("layer 1 weights: ", mat.Formatted(layer_1.Weights))
+	fmt.Println("layer 1 biases: ", mat.Formatted(layer_1.Biases))
+
+	if layer_1 == nil || layer_2 == nil {
+		t.Errorf("error: layer_1 & layer_2 are nil!")
+	}
 }
