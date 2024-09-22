2ee8c77a03b24bd5a8a74514e9a9e5c203d3afc6
saent-x <torsxperience@outlook.com>
2024-09-22 19:20:58 -0400

    feat: wrote tests for dropout layer

--- go-implementation/core/layer/layer_test.go
+++ go-implementation/core/layer/layer_test.go
@@ -2,8 +2,6 @@ package layer
 
 import (
 	"fmt"
-	"github.com/saent-x/ids-nn/core/activation"
-	"github.com/saent-x/ids-nn/core/optimization"
 	"gonum.org/v1/gonum/mat"
 	"testing"
 )
@@ -38,32 +36,18 @@ func TestReLUBackwardFunction(t *testing.T) {
 	fmt.Printf("DInputs:\n%v\n", mat.Formatted(layer.D_Inputs, mat.Prefix(" "), mat.Squeeze()))
 }
 
-func TestLayerCreation(t *testing.T) {
+func TestMisc(t *testing.T) {
 	X := mat.NewDense(4, 3, []float64{0.7, 0.1, 0.2, 0.1, 0.5, 0.4, 0.02, 0.9, 0.08, 0.02, 0.9, 0.08})
 
-	layer_1 := CreateLayer(3, 3)
-	layer_2 := CreateLayer(3, 3)
-
-	activation_1 := new(activation.ReLU)
-	opt := optimization.CreateStochasticGradientDescent(1.0, 0.02, .5)
-
+	layer_1 := NewDropoutLayer(0.1)
 	layer_1.Forward(X)
+
 	fmt.Println(mat.Formatted(layer_1.Output))
-	fmt.Println(" ")
+}
 
-	activation_1.Forward(layer_1.Output)
-	fmt.Println("activation output: ", mat.Formatted(activation_1.Output))
-
-	activation_1.Backward(activation_1.Output)
-	fmt.Println("activation dinputs: ", mat.Formatted(activation_1.D_Inputs))
-	layer_1.Backward(activation_1.D_Inputs)
-
-	fmt.Println("layer 1 dinputs: ", mat.Formatted(layer_1.D_Inputs))
-
-	opt.UpdateParams(layer_1)
-	fmt.Println(" ")
-	fmt.Println("layer 1 weights: ", mat.Formatted(layer_1.Weights))
-	fmt.Println("layer 1 biases: ", mat.Formatted(layer_1.Biases))
+func TestLayerCreation(t *testing.T) {
+	layer_1 := CreateLayer(3, 3, 0, 0, 0, 0)
+	layer_2 := CreateLayer(3, 3, 0, 0, 0, 0)
 
 	if layer_1 == nil || layer_2 == nil {
 		t.Errorf("error: layer_1 & layer_2 are nil!")
