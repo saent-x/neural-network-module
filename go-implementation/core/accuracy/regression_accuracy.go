c476f7212355c5a0dd311d9eb0abe095c487fcd2
saent-x <torsxperience@outlook.com>
2024-10-04 00:59:08 +0100

    feat: implemented regression accuracy func for regression training

--- go-implementation/core/accuracy/regression_accuracy.go
+++ go-implementation/core/accuracy/regression_accuracy.go
@@ -15,19 +15,29 @@ type RegressionAccuracy struct {
 func NewRegressionAccuracy() *RegressionAccuracy {
 	return &RegressionAccuracy{Precision: 0}
 }
-func (ra *RegressionAccuracy) Init(y *mat.Dense, reinit bool) {
-	if ra.Precision == 0 || reinit {
-		ra.Precision = stat.StdDev(y.RawMatrix().Data, nil) / 250
+func (regressionAccuracy *RegressionAccuracy) Init(y *mat.Dense, reinit bool) {
+	if regressionAccuracy.Precision == 0 || reinit {
+		regressionAccuracy.Precision = stat.StdDev(y.RawMatrix().Data, nil) / 250
 	}
 }
 
-func (ra *RegressionAccuracy) Compare(predictions, y *mat.Dense) *mat.Dense {
+func (regressionAccuracy *RegressionAccuracy) Calculate(outputs *mat.Dense, y *mat.Dense) float64 {
+	comparisons := regressionAccuracy.Compare(outputs, y)
+	accuracy := stat.Mean(comparisons.RawMatrix().Data, nil)
+
+	regressionAccuracy.AccumulatedSum += mat.Sum(comparisons)
+	regressionAccuracy.AccumulatedCount += float64(comparisons.RawMatrix().Rows)
+
+	return accuracy
+}
+
+func (regressionAccuracy *RegressionAccuracy) Compare(predictions, y *mat.Dense) *mat.Dense {
 	result := mat.NewDense(y.RawMatrix().Rows, y.RawMatrix().Cols, nil)
 
 	for i := 0; i < y.RawMatrix().Rows; i++ {
 		for j := 0; j < y.RawMatrix().Cols; j++ {
 			sub := math.Abs(predictions.At(i, j) - y.At(i, j))
-			if sub < ra.Precision {
+			if sub < regressionAccuracy.Precision {
 				result.Set(i, j, 1)
 			} else {
 				result.Set(i, j, 0)
