904f4176b9b74387bb0c60c16edc6666392e746d
saent-x <torsxperience@outlook.com>
2024-10-04 00:58:29 +0100

    feat: implemented categorical accuracy func for binary & classification training

--- go-implementation/core/accuracy/categorical_accuracy.go
+++ go-implementation/core/accuracy/categorical_accuracy.go
@@ -1,45 +1,51 @@
 package accuracy
 
 import (
-	"github.com/samber/lo"
+	"github.com/saent-x/ids-nn/core"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
 )
 
 type CategoricalAccuracy struct {
+	Accuracy
 }
 
-func (ra *CategoricalAccuracy) Init(y *mat.Dense, reinit bool) {
+func (categoricalAccuracy *CategoricalAccuracy) Init(y *mat.Dense, reinit bool) {
 
 }
-func (ca *CategoricalAccuracy) Compare(outputs *mat.Dense, y *mat.Dense) *mat.Dense {
+
+func (categoricalAccuracy *CategoricalAccuracy) Calculate(outputs *mat.Dense, y *mat.Dense) float64 {
+	comparisons := categoricalAccuracy.Compare(outputs, y)
+	accuracy := stat.Mean(comparisons.RawMatrix().Data, nil)
+
+	categoricalAccuracy.AccumulatedSum += mat.Sum(comparisons)
+	categoricalAccuracy.AccumulatedCount += float64(comparisons.RawMatrix().Cols)
+
+	return accuracy
+}
+func (categoricalAccuracy *CategoricalAccuracy) Compare(predictions *mat.Dense, y *mat.Dense) *mat.Dense {
 	// get index of max value in each row in softmax
-	p_rows, _ := outputs.Dims()
-	predictions := mat.NewDense(p_rows, 1, nil)
-
-	for i := 0; i < p_rows; i++ {
-		row := outputs.RawRowView(i)
-		predictions.SetRow(i, []float64{float64(lo.IndexOf(row, lo.Max(row)))})
-	}
+	rows, cols := predictions.Dims()
 
 	// check if class-targets are one-hot encoded - convert them to sparse data
-	c_rows, _ := y.Dims()
-	if c_rows > 1 {
-		OHE_class_targets := mat.NewDense(p_rows, 1, nil)
+	y_rows, _ := y.Dims()
 
-		for i := 0; i < c_rows; i++ {
-			var arr []float64
-			row := y.RawRowView(i)
-			OHE_class_targets.SetRow(i, append(arr, float64(lo.IndexOf(row, lo.Max(row)))))
-		}
-		y = mat.DenseCopyOf(OHE_class_targets)
+	if y_rows > 1 {
+		y = mat.DenseCopyOf(core.OHEToSparse(y))
 	}
 
-	pred_class_evaluation := mat.NewDense(p_rows, 1, nil)
+	results := mat.NewDense(rows, cols, nil)
 	// assign 1 where predictions == y, then find the cumulative mean
-	for i, pred := range predictions.RawMatrix().Data {
-		val := lo.Ternary[float64](pred == y.RawMatrix().Data[i], 1, 0)
-		pred_class_evaluation.SetRow(i, []float64{val})
+
+	for i := 0; i < rows; i++ {
+		for j := 0; j < cols; j++ {
+			if predictions.At(i, j) == y.At(i, j) {
+				results.Set(i, j, 1)
+			} else {
+				results.Set(i, j, 0)
+			}
+		}
 	}
 
-	return pred_class_evaluation
+	return results
 }
