22e295a68e70f4139ca8390767853481123ecf50
saent-x <torsxperience@outlook.com>
2024-10-07 20:18:57 +0100

    misc: project structure update

--- go-implementation/core/activation/activation.go
+++
@@ -1,7 +0,0 @@
-package activation
-
-import "gonum.org/v1/gonum/mat"
-
-type IActivation interface {
-	Predictions(outputs *mat.Dense) *mat.Dense
-}
--- go-implementation/core/datamodels/model_parameter.go
+++
@@ -1,8 +0,0 @@
-package datamodels
-
-import "gonum.org/v1/gonum/mat"
-
-type ModelParameter struct {
-	Weights *mat.Dense
-	Biases  *mat.Dense
-}
