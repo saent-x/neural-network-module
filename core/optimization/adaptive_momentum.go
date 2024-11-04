69640b8f4109cdd048f03fefb98fea3470419afb
saent-x <torsxperience@outlook.com>
2024-11-04 12:07:18 +0000

    feat: added func to clip gradients

--- core/optimization/adaptive_momentum.go
+++ core/optimization/adaptive_momentum.go
@@ -11,9 +11,10 @@ type AdaptiveMomentum struct {
 	Epsilon float64
 	Beta_1  float64
 	Beta_2  float64
+	MaxNorm float64
 }
 
-func CreateAdaptiveMomentum(learning_rate float64, decay float64, epsilon float64, beta_1 float64, beta_2 float64) *AdaptiveMomentum {
+func CreateAdaptiveMomentum(learning_rate float64, decay float64, epsilon float64, beta_1 float64, beta_2 float64, max_norm float64) *AdaptiveMomentum {
 
 	adam := new(AdaptiveMomentum)
 
@@ -24,17 +25,18 @@ func CreateAdaptiveMomentum(learning_rate float64, decay float64, epsilon float6
 	adam.Epsilon = epsilon
 	adam.Beta_1 = beta_1
 	adam.Beta_2 = beta_2
+	adam.MaxNorm = max_norm
 
 	return adam
 }
 
-func (self *AdaptiveMomentum) PreUpdateParams() {
-	if self.Decay != 0 {
-		self.CurrentLearningRate = self.LearningRate * (1. / (1. + self.Decay*self.Iterations))
+func (adaptiveMomentum *AdaptiveMomentum) PreUpdateParams() {
+	if adaptiveMomentum.Decay != 0 {
+		adaptiveMomentum.CurrentLearningRate = adaptiveMomentum.LearningRate * (1. / (1. + adaptiveMomentum.Decay*adaptiveMomentum.Iterations))
 	}
 }
 
-func (self *AdaptiveMomentum) UpdateParams(layer *layer.Layer) {
+func (adaptiveMomentum *AdaptiveMomentum) UpdateParams(layer *layer.Layer) {
 	var new_weights, new_biases mat.Dense
 
 	if layer.Weights_Cache == nil || layer.Biases_Cache == nil {
@@ -52,17 +54,17 @@ func (self *AdaptiveMomentum) UpdateParams(layer *layer.Layer) {
 	var dweights_beta, dbiases_beta, weights_momentum_beta, bias_momentum_beta mat.Dense
 
 	dweights_beta.Apply(func(i, j int, v float64) float64 {
-		return (1 - self.Beta_1) * v
+		return (1 - adaptiveMomentum.Beta_1) * v
 	}, layer.D_Weights)
 	dbiases_beta.Apply(func(i, j int, v float64) float64 {
-		return (1 - self.Beta_1) * v
+		return (1 - adaptiveMomentum.Beta_1) * v
 	}, layer.D_Biases)
 
 	weights_momentum_beta.Apply(func(i, j int, v float64) float64 {
-		return self.Beta_1 * v
+		return adaptiveMomentum.Beta_1 * v
 	}, layer.Weights_Momentum)
 	bias_momentum_beta.Apply(func(i, j int, v float64) float64 {
-		return self.Beta_1 * v
+		return adaptiveMomentum.Beta_1 * v
 	}, layer.Biases_Momentum)
 
 	layer.Weights_Momentum.Add(&weights_momentum_beta, &dweights_beta)
@@ -71,26 +73,26 @@ func (self *AdaptiveMomentum) UpdateParams(layer *layer.Layer) {
 	// corrected momentums
 	var weight_momentums_corrected, bias_momentum_corrected mat.Dense
 	weight_momentums_corrected.Apply(func(i, j int, v float64) float64 {
-		return v / (1 - math.Pow(self.Beta_1, self.Iterations+1))
+		return v / (1 - math.Pow(adaptiveMomentum.Beta_1, adaptiveMomentum.Iterations+1))
 	}, layer.Weights_Momentum)
 	bias_momentum_corrected.Apply(func(i, j int, v float64) float64 {
-		return v / (1 - math.Pow(self.Beta_1, self.Iterations+1))
+		return v / (1 - math.Pow(adaptiveMomentum.Beta_1, adaptiveMomentum.Iterations+1))
 	}, layer.Biases_Momentum)
 
 	var dweights_sqr_beta, dbiases_sqr_beta, weight_cache_beta_2, bias_cache_beta_2 mat.Dense
 
 	dweights_sqr_beta.Apply(func(i, j int, v float64) float64 {
-		return (1 - self.Beta_2) * math.Pow(v, 2)
+		return (1 - adaptiveMomentum.Beta_2) * math.Pow(v, 2)
 	}, layer.D_Weights)
 	dbiases_sqr_beta.Apply(func(i, j int, v float64) float64 {
-		return (1 - self.Beta_2) * math.Pow(v, 2)
+		return (1 - adaptiveMomentum.Beta_2) * math.Pow(v, 2)
 	}, layer.D_Biases)
 
 	weight_cache_beta_2.Apply(func(i, j int, v float64) float64 {
-		return self.Beta_2 * v
+		return adaptiveMomentum.Beta_2 * v
 	}, layer.Weights_Cache)
 	bias_cache_beta_2.Apply(func(i, j int, v float64) float64 {
-		return self.Beta_2 * v
+		return adaptiveMomentum.Beta_2 * v
 	}, layer.Biases_Cache)
 
 	layer.Weights_Cache.Add(&weight_cache_beta_2, &dweights_sqr_beta)
@@ -99,27 +101,27 @@ func (self *AdaptiveMomentum) UpdateParams(layer *layer.Layer) {
 	// Corrected Cache
 	var weights_cache_corrected, bias_cache_corrected mat.Dense
 	weights_cache_corrected.Apply(func(i, j int, v float64) float64 {
-		return v / (1 - math.Pow(self.Beta_2, self.Iterations+1))
+		return v / (1 - math.Pow(adaptiveMomentum.Beta_2, adaptiveMomentum.Iterations+1))
 	}, layer.Weights_Cache)
 	bias_cache_corrected.Apply(func(i, j int, v float64) float64 {
-		return v / (1 - math.Pow(self.Beta_2, self.Iterations+1))
+		return v / (1 - math.Pow(adaptiveMomentum.Beta_2, adaptiveMomentum.Iterations+1))
 	}, layer.Biases_Cache)
 
 	// Vanilla SGD
 	new_weights.Apply(func(i, j int, v float64) float64 {
-		return -self.CurrentLearningRate * v
+		return -adaptiveMomentum.CurrentLearningRate * v
 	}, &weight_momentums_corrected)
 	new_biases.Apply(func(i, j int, v float64) float64 {
-		return -self.CurrentLearningRate * v
+		return -adaptiveMomentum.CurrentLearningRate * v
 	}, &bias_momentum_corrected)
 
 	var weights_epsilon_sum, biases_epsilon_sum mat.Dense
 
 	weights_epsilon_sum.Apply(func(i, j int, v float64) float64 {
-		return math.Sqrt(v) + self.Epsilon
+		return math.Sqrt(v) + adaptiveMomentum.Epsilon
 	}, &weights_cache_corrected)
 	biases_epsilon_sum.Apply(func(i, j int, v float64) float64 {
-		return math.Sqrt(v) + self.Epsilon
+		return math.Sqrt(v) + adaptiveMomentum.Epsilon
 	}, &bias_cache_corrected)
 
 	var weights_div, biases_div mat.Dense
@@ -132,10 +134,49 @@ func (self *AdaptiveMomentum) UpdateParams(layer *layer.Layer) {
 	weights_sum.Add(layer.Weights, &weights_div)
 	biases_sum.Add(layer.Biases, &biases_div)
 
+	// clip gradients
+	//adaptiveMomentum.ClipGradients(layer)
+
 	layer.Weights = mat.DenseCopyOf(&weights_sum)
 	layer.Biases = mat.DenseCopyOf(&biases_sum)
 }
 
-func (self *AdaptiveMomentum) PostUpdateParams() {
-	self.Iterations += 1
+func (adaptiveMomentum *AdaptiveMomentum) ClipGradients(layer *layer.Layer) {
+	if adaptiveMomentum.MaxNorm <= 0 {
+		return
+	}
+
+	// Calculate norms
+	weightNorm := mat.Norm(layer.D_Weights, 2)
+	biasNorm := mat.Norm(layer.D_Biases, 2)
+
+	// Clip weights if necessary
+	if weightNorm > adaptiveMomentum.MaxNorm {
+		scale := adaptiveMomentum.MaxNorm / weightNorm
+		rows, cols := layer.D_Weights.Dims()
+
+		// Create a new scaled matrix
+		scaled := mat.NewDense(rows, cols, nil)
+		scaled.Scale(scale, layer.D_Weights)
+
+		// Update the original weights
+		layer.D_Weights.Copy(scaled)
+	}
+
+	// Clip biases if necessary
+	if biasNorm > adaptiveMomentum.MaxNorm {
+		scale := adaptiveMomentum.MaxNorm / biasNorm
+		rows, cols := layer.D_Biases.Dims()
+
+		// Create a new scaled matrix
+		scaled := mat.NewDense(rows, cols, nil)
+		scaled.Scale(scale, layer.D_Biases)
+
+		// Update the original biases
+		layer.D_Biases.Copy(scaled)
+	}
+}
+
+func (adaptiveMomentum *AdaptiveMomentum) PostUpdateParams() {
+	adaptiveMomentum.Iterations += 1
 }
